a391d42cb69cfbb19136ab3533536a414805c7f7
QQianway <qqianway@gmail.com>
2017-11-01 14:20:43 +0800

    Add files via upload

--- web/php/insertRentalMaster.php
+++ web/php/insertRentalMaster.php
@@ -1,66 +1,57 @@
-	<?php
-		include('config.php');
-		
-		if(isset($_POST['companyName']))
-		{
-			$username = $_POST['companyName'];
-			$establishDate = $_POST['establishDate'];
-			$address1 = $_POST['address1'];
-			$address2 = $_POST['address2'];
-			$postcode = $_POST['postcode'];
-			$country = $_POST['country'];
-			$state = $_POST['state'];
-			$city = $_POST['city'];
-			$cemail = $_POST['CEMail'];
-			$cphone = $_POST['CPhone'];
-			$title=$_POST['title'];
-		    $firstName =$_POST['firstName'];
-		    $lastName = $_POST["lastName"];
-		    $gender =$_POST['gender'];
-		    $position = $_POST['position'];
-		    $email = $_POST['EMail'];
-		    $phone = $_POST['Phone'];
-		    $password = $_POST['password'];
-		}
+<html>
+	<head></head>	
+	<body>
+		<script>
+			function repeat()
+	    	{
+	    		alert("Company Email or User Email has used");
+	    	}
+			<?php
+				include('config.php');
+				
+				if(isset($_POST['companyName']))
+				{
+					$username = $_POST['companyName'];
+					$establishDate = $_POST['establishDate'];
+					$address1 = $_POST['address1'];
+					$address2 = $_POST['address2'];
+					$postcode = $_POST['postcode'];
+					$country = $_POST['country'];
+					$state = $_POST['state'];
+					$city = $_POST['city'];
+					$cemail = $_POST['CEMail'];
+					$cphone = $_POST['CPhone'];
+					$title=$_POST['title'];
+				    $firstName =$_POST['firstName'];
+				    $lastName = $_POST["lastName"];
+				    $gender =$_POST['gender'];
+				    $position = $_POST['position'];
+				    $email = $_POST['EMail'];
+				    $phone = $_POST['Phone'];
+				    $password = $_POST['password'];
+				}
 
-	    $checkEmail = "select * from rentalmaster where cemail='$cemail'";
-	    $check = mysqli_query($conn,$checkEmail) or trigger_error($conn->error."[$checkEmail]");
-	    $existsEmail = mysqli_num_rows($check);
-	    if($existsEmail==1 || $existsEmail>1)
-	    {
-	    	?>
-	    	<html>
-	    	<head>
-	    	</head>
-	    	<body onLoad="repeat();">
-	    		
-	    	<script>
-	    		function repeat()
-	    		{
-	    			alert("Company Message has used");
-	    		}
-	    	</script>
-	    	</body>
-	    	</html>
-
-	    	<?php
-	    }
-	    else
-	    {
-	    	$sql = "insert into rentalmaster(username,firstName,lastName, password, email, address1, address2, postcode, country, phone, state, city, cemail, cphone, title, gender, position, establishDate) values('$username','$firstName','$lastName', '$password', '$email', '$address1', '$address2', '$postcode', '$country', '$phone', '$state', '$city', '$cemail', '$cphone', '$title', '$gender', '$position', '$establishDate')";
-	    	$result=mysqli_query($conn,$sql) or trigger_error($conn->error."[$sql]");
-	    }
+			    $checkEmail = "select * from rentalmaster where cemail='$cemail' or email='$email'";
+			    $check = mysqli_query($conn,$checkEmail) or trigger_error($conn->error."[$checkEmail]");
+			    $existsEmail = mysqli_num_rows($check);
+			    if($existsEmail==1 || $existsEmail>1)
+			    {
+			    	?>
+			    	window.onload=repeat;
+			    	<?php
+			    }
+			    else
+			    {
+			    	$sql = "insert into rentalmaster(username,firstName,lastName, password, email, address1, address2, postcode, country, phone, state, city, cemail, cphone, title, gender, position, establishDate) values('$username','$firstName','$lastName', '$password', '$email', '$address1', '$address2', '$postcode', '$country', '$phone', '$state', '$city', '$cemail', '$cphone', '$title', '$gender', '$position', '$establishDate')";
+			    	$result=mysqli_query($conn,$sql) or trigger_error($conn->error."[$sql]");
+			    }
 
 
-            // $sql = "insert into rentalmaster(username,firstName,lastName, password, email,address1, address2, postcode,country,phone,state,city,cemail,cphone,title,gender,position,establishDate) values('test','first','last','12345678','rentalmaster@gmail.com','address1','address2','postcode','country','0123546789','state','city','cemail','0123456789','title','gender','position','2017-10-29')";
+		            // $sql = "insert into rentalmaster(username,firstName,lastName, password, email,address1, address2, postcode,country,phone,state,city,cemail,cphone,title,gender,position,establishDate) values('test','first','last','12345678','rentalmaster@gmail.com','address1','address2','postcode','country','0123546789','state','city','cemail','0123456789','title','gender','position','2017-10-29')";
 
 
-	    mysqli_close($conn);
-	 ?>
-	 <!-- 	 if success direct to other page
- -->
-
-
-
-	</BODY>
-	</HTML>
+			    mysqli_close($conn);
+			 ?>
+		</script>
+	</body>
+</html>
--- web/php/insertUser.php
+++ web/php/insertUser.php
@@ -1,45 +1,55 @@
-
-<?php
-    include ("config.php");
-	$userName = $_POST['username'];
-	$gender = $_POST['gender'];
-	$date = $_POST['gender'];
-	$password = $_POST['password'];
-	$email = $_POST['EMail'];
-	$phone = $_POST['Phone'];
-	$level = 'user';
-
-	$checkEmail = "select * from user where email='$email'";
-	$check = mysqli_query($conn,$checkEmail) or trigger_error($conn->error."[$checkEmail]");
-	$existsEmail = mysqli_num_rows($check);
-	if($existsEmail==1 || $existsEmail>1)
-	{
-	    ?>
-	    <html>
-	    <head>
-	    </head>
-	    <body onLoad="repeat();">
-	    	
-	    <script>
-	    	function repeat()
-	    	{
-	    		alert("User Message has used");
-	    	}
-	    </script>
-	    </body>
-	    </html>
-
+<html>
+	<head></head>
+	<body>
+		<script>
+			function repeat()
+			{
+				alert("User Message has used");
+			}
+			function success()
+			{
+				alert("Register Success");
+			}
 		<?php
-	}
-	  	// $sql = "insert into user(username, gender, birthDate,password, email, phone, level) values ('admin','male','123','123456','email','0123456789','admin')" ; 
-	  	// $sql2 = "insert into user(username, gender, birthDate, password, email, phone, level) values ('user1','male','123','123456','email','0123456789','user')" ;
-	    // $result=mysqli_query($conn,$sql) or trigger_error($conn->error."[$sql]");
-	    // $result2=mysqli_query($conn,$sql2) or trigger_error($conn->error."[$sql]");
-	else
-	{
-	    $sql = "insert into user(username, gender,date, password, email, phone, level) values ('$userName','$gender','$date',$password','$email','$phone','$level')" ;
-	    $result=mysqli_query($conn,$sql) or trigger_error($conn->error."[$sql]");
-	}
+		    include ("config.php");
+			$userName = $_POST['username'];
+			$gender = $_POST['gender'];
+			$date = $_POST['gender'];
+			$password = $_POST['password'];
+			$email = $_POST['EMail'];
+			$phone = $_POST['Phone'];
+			$level = 'user';
 
-	    mysqli_close($conn);
-?>
+			$checkEmail = "select * from user where email='$email'";
+			$check = mysqli_query($conn,$checkEmail) or trigger_error($conn->error."[$checkEmail]");
+			$existsEmail = mysqli_num_rows($check);
+			if($existsEmail==1 || $existsEmail>1)
+			{
+			    ?>
+			    window.onLoad=repeat;	
+				<?php
+			}
+			  	// $sql = "insert into user(username, gender, birthDate,password, email, phone, level) values ('admin','male','123','123456','email','0123456789','admin')" ; 
+			  	// $sql2 = "insert into user(username, gender, birthDate, password, email, phone, level) values ('user1','male','123','123456','email','0123456789','user')" ;
+			    // $result=mysqli_query($conn,$sql) or trigger_error($conn->error."[$sql]");
+			    // $result2=mysqli_query($conn,$sql2) or trigger_error($conn->error."[$sql]");
+			else
+			{
+			    $sql = "insert into user(username, gender,date, password, email, phone, level) values ('$userName','$gender','$date',$password','$email','$phone','$level')" ;
+			    $result=mysqli_query($conn,$sql) or trigger_error($conn->error."[$sql]");
+			    if($result)
+			    {
+			    	header('Location:..\WEB-INF\LoginSuccess.html');
+			    }
+			    else
+			    {
+			    	?>
+			    	window.onload=success;
+			    	<?php
+			    }
+			}
+			mysqli_close($conn);
+		?>
+		</script>
+	</body>
+</html>
